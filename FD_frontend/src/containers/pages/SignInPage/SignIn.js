c3a357b995190daec6ec5ed292e72f3cbe868283
Min Whoo Lee <lionminhu@hotmail.com>
2019-05-01 17:36:48 +0900

    SignIn container file

---
+++ FD_frontend/src/containers/pages/SignInPage/SignIn.js
@@ -0,0 +1,20 @@
+import {connect} from 'react-redux'
+import {gotoSignUp, requestSignIn} from '../../../store/SignInPage/actions'
+import SignIn from '../../../components/pages/SignInPage/molecules/SignIn'
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClickSignUp: () => {
+      dispatch(gotoSignUp())
+    },
+    onClickSignIn: (id, password) => {
+      requestSignIn(id, password)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
