0c276cc6f41c4fefc71ada75c5718586da462b81
Min Whoo Lee <lionminhu@hotmail.com>
2019-05-03 22:43:12 +0900

    Trying to implement signInAsync

--- FD_frontend/src/store/SignInPage/sagas.js
+++ FD_frontend/src/store/SignInPage/sagas.js
@@ -1,19 +1,48 @@
 import { takeEvery, all, take, put, call, fork } from 'redux-saga/effects'
 import api from 'services/api'
 import * as actions from '../actions'
+import * as ACTIONTYPES from './actionTypes'
 
 export function* signInAsync({email, password}) {
+  console.log('store/SignInPage/sagas signInAsync 1')
+  console.log(email)
+  console.log(password)
+  console.log('store/SignInPage/sagas signInAsync 2')
   try {
-    // TODO
+    console.log('store/SignInPage/sagas signInAsync 3')
+    const data = {
+      username: "test_username",
+      email: email,
+      password: password,
+    }
+    console.log('store/SignInPage/sagas signInAsync 4')
+    console.log(data)
+    console.log('store/SignInPage/sagas signInAsync 5')
+    const response = yield call(api.post, "http://127.0.0.1:8000/api/rest-auth/login/", data)
+    console.log('store/SignInPage/sagas signInAsync 6')
+    console.log(response)
+    console.log('store/SignInPage/sagas signInAsync 7')
+    yield put(actions.signInSuccess("test_token"))
+    console.log('store/SignInPage/sagas signInAsync 8')
   } catch(e) {
+    console.log('store/SignInPage/sagas signInAsync 9')
+    console.log(e)
+    console.log('store/SignInPage/sagas signInAsync 10')
     console.error(e)
+    console.log('store/SignInPage/sagas signInAsync 11')
+    yield put(actions.signInFailed())
+    console.log('store/SignInPage/sagas signInAsync 12')
   }
 }
 
 export function* watchRequestSignIn() {
+  console.log('store/SignInPage/sagas watchRequestSignIn 1')
   yield takeEvery(ACTIONTYPES.REQUEST_SIGN_IN, signInAsync)
+  console.log('store/SignInPage/sagas watchRequestSignIn 2')
 }
 
 export default function* () {
+  console.log('store/SignInPage/sagas 1')
   yield fork(watchRequestSignIn)
+  console.log('store/SignInPage/sagas 2')
 }
