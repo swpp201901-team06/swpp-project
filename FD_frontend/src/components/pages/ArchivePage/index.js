db5d56457da19b52156f9ce73fe175c26d7844fa
Min Whoo Lee <lionminhu@hotmail.com>
2019-05-04 17:52:45 +0900

    No need for tempmolecule

--- FD_frontend/src/components/pages/ArchivePage/index.js
+++ FD_frontend/src/components/pages/ArchivePage/index.js
@@ -1,18 +1,4 @@
 import React from 'react'
-import TempMolecule from '../../../containers/pages/ArchivePage/TempMolecule'
-
-/*
-const ArchivePage = ({nickname}) => {
-  console.log('components/pages/ArchivePage/index ArchivePage 1')
-  console.log(nickname)
-  console.log('components/pages/ArchivePage/index ArchivePage 2')
-  return (
-    <div>
-      <TempMolecule />
-    </div>
-  )
-}
-*/
 
 const ArchivePage = React.createClass({
   render () {
