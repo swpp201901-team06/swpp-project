838249f8f4dac3fb43cf7b2c374323cd5e0d8676
한상현 <hansanghyeon@hansanghyeon-ui-MacBook-Pro.local>
2019-05-04 16:40:05 +0900

    redirect code made need to test with test page

--- FD_frontend/src/containers/MainPage/Main.js
+++ FD_frontend/src/containers/MainPage/Main.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux'
 import { Main } from '../../components/pages/MainPage/Molecules/Main'
 import { goToSignIn, goToGuest } from '../../store/Mainpage/actions'
+import { baseHistory } from '../../index'
 
 const mapStateToProps = (state) =>{
     return {
@@ -11,9 +12,11 @@ const mapStateToProps = (state) =>{
 const mapDispatchToProps = (dispatch) => {
     return {
         onClickSignin: () => {
+            baseHistory.push('/signin')
             dispatch(goToSignIn())
         },
         onClickGuest: () => {
+            baseHistory.push('/guest')
             dispatch(goToGuest())
         }
     }
