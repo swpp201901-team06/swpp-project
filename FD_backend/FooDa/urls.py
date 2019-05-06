5ebf22e088c01f44ef62b5a29673af6f744c07ea
1gkswogns <1gkswogns@naver.com>
2019-05-06 17:58:28 +0900

    adding test1

--- FD_backend/FooDa/urls.py
+++ FD_backend/FooDa/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
 
     path('archivelist/', views.ArchiveListView.as_view(), name = 'archivelist'),
-    path('archivedetail/<str:username>/', views.ArchiveVisitorIncreaseView.as_view(), name = 'archivedetail'),
+#    path('archivedetail/<str:username>/', views.ArchiveVisitorIncreaseView.as_view(), name = 'archivedetail'),
 
     path('restaurantlist/', views.RestaurantListView.as_view(), name = 'restaurantlist'),
     path('restaurantdetail/<int:pk>/', views.RestaurantDetailView.as_view(), name = 'restaurantdetail'),
@@ -22,9 +22,9 @@ urlpatterns = [
     path('taggeditem/', views.TaggedItemList.as_view(), name = 'taggeditem_tag'),
     path('tagfilter/<int:tag_id>', views.TagFilter.as_view(), name = 'tagfilter'),
 
-    path('guestCommentlist/', views.GuestCommentListView.as_view(), name = 'guestCommentlist'),
-    path('guestCommentdetail/<int:pk>/', views.GuestCommentDetailView.as_view(), name = 'guestCommentdetail'),
+#    path('guestCommentlist/', views.GuestCommentListView.as_view(), name = 'guestCommentlist'),
+#    path('guestCommentdetail/<int:pk>/', views.GuestCommentDetailView.as_view(), name = 'guestCommentdetail'),
 
-    path('weeklyrankinglist/', views.WeeklyRankingListView.as_view(), name = 'weeklyrankinglist'),
-    path('weeklyrankingdetail/<int:pk>/', views.WeeklyRankingDetailView.as_view(), name = 'weeklyrankingdetail'),
+#    path('weeklyrankinglist/', views.WeeklyRankingListView.as_view(), name = 'weeklyrankinglist'),
+#    path('weeklyrankingdetail/<int:pk>/', views.WeeklyRankingDetailView.as_view(), name = 'weeklyrankingdetail'),
 ]
