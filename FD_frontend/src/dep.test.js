82251457d2650461752482fc0caf8c3d2d732bf7
Min Whoo Lee <lionminhu@hotmail.com>
2019-05-06 14:43:54 +0900

    Jest setup

---
+++ FD_frontend/src/dep.test.js
@@ -0,0 +1,11 @@
+import 'jest-localstorage-mock'
+
+test('should save to localStorage', () => {
+  const KEY = 'test_key'
+  const VAL = 'test_val'
+
+  dispatch(action.update(KEY, VAL))
+  expect(localStorage.setItem).toHaveBeenLastCalledWith(KEY, VAL)
+  expect(localStorage.__STORE__[KEY]).toBe(VAL)
+  expect(Object.keys(localStorage.__STORE__).length).toBe(1)
+})
