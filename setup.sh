76127e8a13af22163ba1ce74bbc8b154be19a2b5
Min Whoo Lee <lionminhu@hotmail.com>
2019-05-12 14:17:31 +0000

    Virtualenv with python3.6, create separate requirements file

--- setup.sh
+++ setup.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 sudo apt install -y virtualenv
-virtualenv env
+virtualenv --python=python3.6 env
 source ./env/bin/activate
 
-sudo pip3 install django==2.2.1
-sudo pip3 install djangorestframework==3.9.3
-sudo pip3 install coverage==4.5.3
-sudo pip3 install requests==2.21.0
-sudo pip3 install django-cors-headers==2.5.3
-sudo pip3 install django-tagging==0.4.6
-sudo pip3 install django-hitcount==1.3.0
-sudo pip3 install django-rest-auth==0.9.5
-sudo pip3 install django-allauth==0.39.1
+sudo pip3 install -r requirements.txt
 
 cd FD_backend
 python3 manage.py makemigrations users
