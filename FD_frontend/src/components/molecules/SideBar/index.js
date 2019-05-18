9c278a838d4e0eae8873d355fdece31c77bb41cf
Min Whoo Lee <lionminhu@hotmail.com>
2019-05-19 00:38:58 +0900

    Fixed really just the brackets of side bar molecule

--- FD_frontend/src/components/molecules/SideBar/index.js
+++ FD_frontend/src/components/molecules/SideBar/index.js
@@ -32,7 +32,7 @@ export const SideBar = ({ statefunction }) => {
     </Link>
   );
   return (
-    {statefunction.SignInPage.isLoggedIn ? loggedInDisplay : notLoggedInDisplay}
+    statefunction.SignInPage.isLoggedIn ? loggedInDisplay : notLoggedInDisplay
 	);
 };
 
