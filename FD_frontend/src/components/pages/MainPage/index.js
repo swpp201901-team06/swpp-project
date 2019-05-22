63dbb355d532ae5cdea8d2e4bebd0798fd93851d
MoBeau <shhan098@naver.com>
2019-05-22 18:07:28 +0900

    div bgcolor need to be whole page

--- FD_frontend/src/components/pages/MainPage/index.js
+++ FD_frontend/src/components/pages/MainPage/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Main from '../../../containers/MainPage/Main'
-
 // import { PageTemplate, Header, Hero, Footer, FeatureList } from 'components'
+
+const bgStyles = {
+  backgroundColor : "#a91935",
+  height : '100%',
+  width : '100%'
+};
 
 const MainPage = () => {
   return (
-    <div>
-      <Main />
+    <div style={bgStyles}>
+      <Main/>
     </div>
+    
+    // <BodyBackgroundColor backgroundColor='#FF00FF'>
+    //     <h1>Home, sweet home.</h1>
+    // </BodyBackgroundColor>
   )
 }
 
