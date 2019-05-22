de268a5e6bf4fd34c26aa307a30d08195e174f4e
theNocturni <psbscribr@gmail.com>
2019-05-23 03:40:50 +0900

    google map component

--- FD_frontend/src/components/pages/ArchivePage/index.js
+++ FD_frontend/src/components/pages/ArchivePage/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 import PostButton from './atoms/PostButton'
 import ArchiveReviewList from '../../../containers/ArchivePage/ReviewList'
 import ArchiveReviewDetail from '../../../containers/ArchivePage/ReviewDetail'
-
+import GoogleMap from '../../molecules/GoogleMap'
 
 const ArchivePage = React.createClass({
   render () {
@@ -22,6 +22,8 @@ const ArchivePage = React.createClass({
         </Link>
         <h1>{' '}</h1>
         <ArchiveReviewDetail />
+	<h1>{' '}</h1>
+	<GoogleMap />
       </div>
     )
   }
