148f780cb440687791e60f93fe3329dc157ab639
MoBeau <shhan098@naver.com>
2019-05-22 23:12:33 +0900

    background color finished

--- FD_frontend/src/components/pages/MainPage/index.js
+++ FD_frontend/src/components/pages/MainPage/index.js
@@ -2,15 +2,9 @@ import React from 'react'
 import Main from '../../../containers/MainPage/Main'
 // import { PageTemplate, Header, Hero, Footer, FeatureList } from 'components'
 
-const bgStyles = {
-  backgroundColor : "#a91935",
-  height : '100%',
-  width : '100%'
-};
-
 const MainPage = () => {
   return (
-    <div style={bgStyles}>
+    <div>
       <Main/>
     </div>
     
