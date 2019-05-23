a126627b5428675f1e39d5d773ce695302d144b6
Min Whoo Lee <lionminhu@hotmail.com>
2019-05-23 12:34:48 +0900

    Revert "Refresh upon sign in"
    This reverts commit 2576d200261bab081b61e429aa336cc90bc071d2.

--- FD_frontend/src/store/SignInPage/sagas.js
+++ FD_frontend/src/store/SignInPage/sagas.js
@@ -26,7 +26,6 @@ export function* signInAsync({ email, password }) {
     yield put(actions.signInSuccess(response.key, email, password, nicknameResponse.username))
     console.log('signInAsync after signInSuccess')
     yield put(push('/' + nicknameResponse.username + '/archive'))
-    window.location.reload()
   } catch (e) {
     console.log('signInAsync error')
     console.error(e)
@@ -50,7 +49,6 @@ export function* watchGotoArchive() {
   while (true) {
     const { nickname } = yield take(actionTypes.GOTO_ARCHIVE)
     yield put(push('/' + nickname + '/archive'))
-    window.location.reload()
   }
 }
 
