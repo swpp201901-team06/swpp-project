54c1f11a16ed3d349fb5491af71d4636d94e4c67
1gkswogns <1gkswogns@naver.com>
2019-06-02 17:03:54 +0900

    modify url get-username

--- FD_backend/users/urls.py
+++ FD_backend/users/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('list', views.UserListView.as_view(), name = 'user_list'),
     path('detail/<str:pk>', views.UserDetailView.as_view(), name = 'user_detail'),
-    path('email/<str:email>',views.GetUserNameView.as_view(), name = 'get_username'),
+    path('username/<str:email>',views.GetUserNameView.as_view(), name = 'get_username'),
 
     path('exists/email/<str:email>', views.UserEmailExistView.as_view(), name = 'user_email'),
     path('exists/username/<str:username>', views.UserNameExistView.as_view(), name = 'user_name'),
