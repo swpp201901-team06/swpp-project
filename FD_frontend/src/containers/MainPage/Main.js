fc958b51697da5ed88af4f62fa4b3c4faecd97a2
Min Whoo Lee <lionminhu@hotmail.com>
2019-06-16 00:02:21 +0900

    Main container tests

--- FD_frontend/src/containers/MainPage/Main.js
+++ FD_frontend/src/containers/MainPage/Main.js
@@ -2,13 +2,13 @@ import { connect } from 'react-redux'
 import Main from '../../components/pages/MainPage/Molecules/Main/index'
 import { gotoSignIn, gotoGuest } from '../../store/MainPage/actions'
 
-const mapStateToProps = (state) => {
+export const mapStateToProps = (state) => {
   return {
     statefunction: state,
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+export const mapDispatchToProps = (dispatch) => {
   return {
     onClickSignIn: () => {
       dispatch(gotoSignIn())
