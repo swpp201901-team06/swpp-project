262ed9ec17bcc0a3c99e8455802855f2b441bf8e
1gkswogns <1gkswogns@naver.com>
2019-06-17 14:13:40 +0900

    add restaurant search

--- FD_backend/restaurant/urls.py
+++ FD_backend/restaurant/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('list', views.RestaurantListView.as_view(), name = 'restaurant_list'),
     # 특정한 식당 정보
     path('detail/<int:pk>', views.RestaurantDetailView.as_view(), name = 'restaurant_detail'),
+
+    path('search/<str:name>', views.SearchedRestaurantListView.as_view())
 ]
