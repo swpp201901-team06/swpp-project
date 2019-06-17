a214d6191dfc440e8d928ad0ab1285fa4bac6e8a
Min Whoo Lee <lionminhu@hotmail.com>
2019-06-17 14:35:44 +0900

    Fixed URLs in SearchPage sagas

--- FD_frontend/src/store/SearchPage/sagas.js
+++ FD_frontend/src/store/SearchPage/sagas.js
@@ -1,19 +1,19 @@
-import { takeEvery, take, put, fork } from 'redux-saga/effects'
+import { takeEvery, put, fork } from 'redux-saga/effects'
 import { push } from 'react-router-redux'
 import { callUrl } from '../sagas'
 import * as actions from './actions'
 
-const backendUrl = 'http://127.0.0.1:8000/'
-const usernameUrl = `${backendUrl}review/search/`
-const tagUrl = `${backendUrl}tag/filter/`
+const backendUrl = 'http://127.0.0.1:8000'
+const usernameUrl = `${backendUrl}/review/search`
+const tagUrl = `${backendUrl}tag/filter`
 
-export function* getResults({ key, value }) {
+export function* getResultsSaga({ key, value }) {
   try {
     let response
     if (key === 'author') {
-      response = yield callUrl('GET', `${usernameUrl}${value}`)
+      response = yield callUrl('GET', `${usernameUrl}/${value}`)
     } else {
-      response = yield callUrl('GET', `${tagUrl}${value}`)
+      response = yield callUrl('GET', `${tagUrl}/${value}`)
     }
     yield put(actions.getResultsSuccess(response))
   } catch (err) {
@@ -23,7 +23,7 @@ export function* getResults({ key, value }) {
 }
 
 export function* watchGetResultsRequest() {
-  yield takeEvery(actions.GET_RESULTS_REQUEST, getResults)
+  yield takeEvery(actions.GET_RESULTS_REQUEST, getResultsSaga)
 }
 
 export default function* () {
