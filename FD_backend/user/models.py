1bfc69ae98d00bf9063e8822ee8d84e8c93f402b
1gkswogns <1gkswogns@naver.com>
2019-06-19 22:51:45 +0900

    modify review,restaurant ranking & search - check user's public status

--- FD_backend/user/models.py
+++ FD_backend/user/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 class CustomUser(AbstractUser):
     username = models.CharField(blank = False, max_length = 20, unique = True)
     email = models.EmailField(blank = False, unique = True)
-    public_status = models.BooleanField(default = False)
+    public_status = models.BooleanField(default = True)
     is_admin = models.BooleanField(default = False)
     is_active = models.BooleanField(default = True)
 
