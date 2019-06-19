7191f935401baaeba01aca4258a0e042494315d6
1gkswogns <1gkswogns@naver.com>
2019-06-19 22:29:49 +0900

    add restaurant name to review

--- FD_backend/restaurant/models.py
+++ FD_backend/restaurant/models.py
@@ -9,3 +9,6 @@ class Restaurant(models.Model):
     address = models.TextField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
